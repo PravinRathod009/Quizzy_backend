e659e491ec5442817a58ef7d1a5c07afaa25620f
pravin <pravinrathod22522@gmail.com>
2026-05-14 20:24:24 +0530

    db atlast setup

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/aiquizzy');
+   const conn = await mongoose.connect('mongodb+srv://shrichavan9211_db_user:EWVtWEesLwyQ8maA@cluster0.xnx16my.mongodb.net/?appName=Cluster0');
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error('❌ MongoDB connection error:', err.message);
